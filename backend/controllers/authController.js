37c0e8728472e53dfac943f721b24f70e80c7f5f
Aleksandra Pawelska <aaspyr@gmail.com>
2024-01-06 17:06:15 +0100

    JWT🔑

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,23 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRED_IN,
+  });
+
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
@@ -12,9 +25,5 @@ exports.signup = catchAsync(async (req, res, next) => {
 });
 
 //   createSendToken(newUser, 201, res);
-// name: req.body.name,
-// email: req.body.email,
-// password: req.body.password,
-// passwordConfirm: req.body.passwordConfirm,
-// passwordChangedAt: req.body.passwordChangedAt,
+
 //});
