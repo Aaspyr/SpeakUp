ce7912d5c47e5caead35e84d6853e94d3a068654
Aleksandra Pawelska <aaspyr@gmail.com>
2024-01-06 15:37:35 +0100

    Unhandled rejection💥

--- backend/server.js
+++ backend/server.js
@@ -22,4 +22,14 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App listening on port ${port}`));
+const server = app.listen(port, () =>
+  console.log(`App listening on port ${port}`)
+);
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled rejection. Shutting down");
+  server.close(() => {
+    process.exit(1);
+  });
+});
