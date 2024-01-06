c722d21a6e980745677d9741a8ad72e946797c26
Aleksandra Pawelska <aaspyr@gmail.com>
2024-01-06 14:20:21 +0100

    Cards Validation 📄

--- backend/models/cardsModel.js
+++ backend/models/cardsModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const validator = require("validator");
 
 const cardsSchema = new mongoose.Schema({
   id: {
@@ -9,14 +11,14 @@ const cardsSchema = new mongoose.Schema({
     required: [true, "A card must have a name"],
     unique: true,
     trim: true,
-  },
-  category: {
-    type: String,
-    required: [true, "A card must have a category"],
+    maxLength: [20, "A card name must have maximum 20 characters"],
+    minLength: [1, "A card name must have at least character"],
   },
   cardsCollection: {
     type: String,
     required: [true, "A card must have a collection"],
+    maxLength: [20, "A card name must have maximum 20 characters"],
+    minLength: [1, "A card name must have at least character"],
   },
   favourites: {
     type: Boolean,
@@ -33,11 +35,15 @@ const cardsSchema = new mongoose.Schema({
     type: String,
     required: [true, "A card must have a image"],
   },
-  images: [String],
   cteatedAt: {
     type: Date,
     default: Date.now(),
   },
+});
+
+cardsSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Card = mongoose.model("Card", cardsSchema);
