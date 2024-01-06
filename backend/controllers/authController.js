335cb8c4c18f90ae6cf5a3fcdf0352aad241f352
Aleksandra Pawelska <aaspyr@gmail.com>
2024-01-06 20:03:10 +0100

    Update User Password🔑

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -172,3 +172,21 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   //4)Log the user in
   createSendToken(user, 200, res);
 });
+
+//UPDATE PASSWORD
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  //1) get the user from the collection
+  const user = await User.findById(req.user.id).select("+password");
+
+  //2) Chceck if POSTed current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError("Incorrect password", 401));
+  }
+  //3) If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  //4) Log the user in,send JTW
+  createSendToken(user, 200, res);
+});
