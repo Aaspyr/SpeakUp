9e4ab42b7e7926537b6a4a177d072db888494701
Aleksandra Pawelska <aaspyr@gmail.com>
2024-01-07 00:08:05 +0100

    message

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 function Login() {
-  return <div>Login Page</div>;
+
+  const (email, setEmail) = useState("")
+  const (password, setPassword) = useState("")
+
+async function submit(e){
+  e.preventDefault()
+  try{
+await axios.post("http://127.0.0.1:3000/api/v1/users/login",{email, password})
+  }catch(e){
+console.log(e)
+  }
+}
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="POST">
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Email"
+          name=""
+          id=""
+        />
+        <input
+          type="password"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Password"
+          name=""
+          id=""
+        />
+        <input type="submit" />
+      </form>
+      <br />
+      <p>OR</p>
+      <br />
+      <Link to="/signup">SignUp</Link>
+    </div>
+  );
 }
 
 export default Login;
