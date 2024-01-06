e0a27ae0f90d479add21b2422271b9198b09af2d
Aleksandra Pawelska <aaspyr@gmail.com>
2024-01-06 14:31:21 +0100

    delete comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Exercise from "./pages/Exercise";
@@ -14,21 +12,12 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import AppLayout from "./ui/AppLayout";
 
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       staleTime: 60 * 1000,
-//     },
-//   },
-// });
-
 function App() {
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          {/*<Route index element={<Navigate raplace to="dashboard"/>}/>*/}
           <Route element={<AppLayout />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="users" element={<Users />} />
